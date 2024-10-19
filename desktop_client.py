4696b447ede6efe52c510e27ad917f0e4f532b47
Ethan Vince-Budan <ethan.vince.budan@gmail.com>
2024-10-19 16:04:48 -0400

    GUI v1

--- desktop_client.py
+++ desktop_client.py
@@ -1,25 +1,28 @@
-from tkinter import ttk, Tk, StringVar, IntVar
+from tkinter import ttk, Tk, StringVar, IntVar, HORIZONTAL
 from pystray import Icon, Menu, MenuItem
-from PIL import Image, ImageDraw
+from PIL import Image
 from serial import Serial
 from serial.tools.list_ports import comports as list_comports
-from pycaw.pycaw import AudioUtilities, AudioDeviceState, IAudioEndpointVolume
-from comtypes import CLSCTX_ALL
-
+from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
+from pycaw.constants import EDataFlow, DEVICE_STATE
+from comtypes import CLSCTX_ALL, COMError
 from threading import Thread, Event
 
 class TrayApplication(Tk):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.iconbitmap('icon.ico')
         self.comport_var = StringVar()
         self.baud_var = IntVar()
-        AudioDeviceUpdater.find_all_devices()
-        self.serial_port = ThreadedPortReader(timeout=0.25)
-        self.all_comports = {}
+        self.device_vars = [StringVar() for i in range(4)]
+        self.all_comports = self.scan_comports()
+        self.all_audio_devices = self.scan_audio_devices()
 
         self.protocol("WM_DELETE_WINDOW", self.withdraw)
 
-        self.scan_comports()
+        self.deviceUpdater = DeviceUpdater(self.all_audio_devices.values())
+        self.serial_port = ThreadedPortReader(self.deviceUpdater, timeout=1)
+
         self.icon = self.create_tray_icon()
         self.create_window_content()
 
@@ -31,17 +34,34 @@ class TrayApplication(Tk):
         self.icon.stop()
 
     def scan_comports(self):
-        self.all_comports = {}
-        for c in list_comports():
-            self.all_comports[str(c)] = c
+        return {str(c): c for c in list_comports()}
 
-    def start_comport(self):
+    def scan_audio_devices(self):
+        deviceEnumerator = AudioUtilities.GetDeviceEnumerator()
+
+        deviceCollection = deviceEnumerator.EnumAudioEndpoints(
+            EDataFlow.eRender.value, DEVICE_STATE.ACTIVE.value
+        )
+        activeDevices = []
+        for i in range(deviceCollection.GetCount()):
+            dev = deviceCollection.Item(i)
+            if dev is not None:
+                activeDevices.append(AudioUtilities.CreateDevice(dev))
+
+        return {d.FriendlyName: d for d in activeDevices}
+
+    def start_controlling(self):
         print(f"Opening COM port {self.comport_var.get()}")
         self.serial_port.port = self.all_comports[self.comport_var.get()].device
         self.serial_port.baudrate = self.baud_var.get()
+        self.deviceUpdater.select_devices([v.get() for v in self.device_vars])
         self.serial_port.open()
         self.serial_port.start()
+
+        for cb in self.deviceCbList:
+            cb.state(['disabled'])
         self.startButton.state(['disabled'])
+        self.refreshButton.state(['disabled'])
         self.portSelect.state(['disabled'])
         self.baudSelect.state(['disabled'])
         self.stopButton.state(['!disabled'])
@@ -50,23 +70,23 @@ class TrayApplication(Tk):
         print("Closing COM port.")
         self.serial_port.stop()
         self.serial_port.close()
+
+        for cb in self.deviceCbList:
+            cb.state(['!disabled'])
         self.stopButton.state(['disabled'])
+        self.refreshButton.state(['!disabled'])
         self.portSelect.state(['!disabled'])
         self.baudSelect.state(['!disabled'])
         self.startButton.state(['!disabled'])
 
-    def create_image(self, width, height, color1, color2):
-        image = Image.new('RGB', (width, height), color1)
-        dc = ImageDraw.Draw(image)
-        dc.rectangle(
-            (width // 2, 0, width, height // 2),
-            fill=color2
-        )
-        dc.rectangle(
-            (0, height // 2, width // 2, height),
-            fill=color2
-        )
-        return image
+    def refresh_device_list(self):
+        self.all_audio_devices = self.scan_audio_devices()
+        for cb in self.deviceCbList:
+            cb['values'] = ["None"] + list(self.all_audio_devices.keys())
+            if cb.get() not in cb['values']:
+                cb.current(0)
+        self.deviceUpdater = DeviceUpdater(self.all_audio_devices.values())
+        self.serial_port = ThreadedPortReader(self.deviceUpdater, timeout=1)
 
     def create_menu(self):
         exitItem = MenuItem("Exit", self.graceful_exit)
@@ -74,41 +94,67 @@ class TrayApplication(Tk):
         return Menu(showItem, exitItem)
 
     def create_tray_icon(self):
-        icon = self.create_image(64, 64, 'black', 'white')
-        menu = self.create_menu()
-        return Icon(self, title="AudioControl", icon=icon, menu=menu)
+        with Image.open('icon.ico') as im:
+            menu = self.create_menu()
+            return Icon(self, title="AudioControl", icon=im, menu=menu)
 
     def create_window_content(self):
-        mainFrame = ttk.Frame(self)
+        mappingFrame = ttk.Labelframe(self, text="Device Mapping")
+        mappingFrame.grid(row=0,column=0, sticky="NSEW", padx=5, pady=5)
 
-        portLabel = ttk.Label(mainFrame, text="COM Port:")
-        portLabel.grid(row=0, column=0)
+        self.deviceCbList = []
+        for i in range(4):
+            cbLabel = ttk.Label(mappingFrame, text=f"D{i}:")
+            cbLabel.grid(row=i,column=0, padx=3)
+            cb = ttk.Combobox(mappingFrame, textvariable=self.device_vars[i],
+                    values = ["None"] + list(self.all_audio_devices.keys()), width=50)
+            cb.state(['readonly'])
+            cb.grid(row=i,column=1, pady=0 if i != 3 else (0,5))
+            cb.current(0)
+            self.deviceCbList.append(cb)
 
-        self.portSelect = ttk.Combobox(mainFrame, textvariable=self.comport_var,
-                values=list(self.all_comports.keys()), width=50)
+        optionsFrame = ttk.Labelframe(self, text="Options")
+        optionsFrame.grid(row=2,column=0, sticky="NSEW", padx=5, pady=5)
+        optionsFrame.columnconfigure(1, weight=1)
+
+        portLabel = ttk.Label(optionsFrame, text="COM Port:")
+        portLabel.grid(row=0, column=0, padx=3)
+
+        self.portSelect = ttk.Combobox(optionsFrame, textvariable=self.comport_var,
+                values=list(self.all_comports.keys()), width=40)
         self.portSelect.state(['readonly'])
         self.portSelect.current(0)
-        self.portSelect.grid(row=0, column=1)
+        self.portSelect.grid(row=0, column=1, sticky="W")
 
-        baudLabel = ttk.Label(mainFrame, text="Baud Rate:")
-        baudLabel.grid(row=1, column=0)
+        baudLabel = ttk.Label(optionsFrame, text="Baud Rate:")
+        baudLabel.grid(row=1, column=0, padx=3, pady=(0,5))
 
-        self.baudSelect = ttk.Combobox(mainFrame, textvariable=self.baud_var,
-                values=[1200,1800,2400,4800,9600,19200])
+        self.baudSelect = ttk.Combobox(optionsFrame, textvariable=self.baud_var,
+                values=[1200,1800,2400,4800,9600,19200], width=15)
         self.baudSelect.state(['readonly'])
-        self.portSelect.current(0)
-        self.baudSelect.grid(row=1, column=1)
+        self.baudSelect.current(0)
+        self.baudSelect.grid(row=1, column=1, sticky="W", pady=(0,5))
 
-        self.startButton = ttk.Button(mainFrame, text="Start",
-                command=self.start_comport)
-        self.startButton.grid(row=2, column=0)
+        buttonFrame = ttk.Frame(self)
+        buttonFrame.grid(row=4,column=0, sticky="NSEW", padx=5, pady=5)
+        buttonFrame.columnconfigure(3, weight=1)
 
-        self.stopButton = ttk.Button(mainFrame, text="Stop",
+        self.startButton = ttk.Button(buttonFrame, text="Start",
+                command=self.start_controlling)
+        self.startButton.grid(row=0, column=0)
+
+        self.stopButton = ttk.Button(buttonFrame, text="Stop",
                 command=self.stop_comport)
         self.stopButton.state(['disabled'])
-        self.stopButton.grid(row=2, column=1)
+        self.stopButton.grid(row=0, column=1)
 
-        mainFrame.pack()
+        self.refreshButton = ttk.Button(buttonFrame, text="Refresh Devices",
+                command=self.refresh_device_list)
+        self.refreshButton.grid(row=0,column=2)
+
+        quitButton = ttk.Button(buttonFrame, text="Quit",
+                command=self.graceful_exit)
+        quitButton.grid(row=0, column=3, sticky="E")
 
     def mainloop(self):
         self.icon.run_detached()
@@ -116,71 +162,69 @@ class TrayApplication(Tk):
 
 
 class ThreadedPortReader(Serial):
-    def __init__(self, *args, **kwargs):
+    def __init__(self, updater, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.readEvent = Event()
-        self.readThread = None
+        self.updater = updater
+        self.readThread = Thread(target=None)
 
     def start(self):
         self.readEvent.set()
-        self.readThread = Thread(target=self.continual_read, args=[self.readEvent])
+        self.readThread = Thread(target=self.continual_read,
+                args=[self.updater.set_device_volumes, self.readEvent])
         self.readThread.start()
 
     def stop(self):
         self.readEvent.clear()
-        self.readThread.join()
+        if self.readThread.is_alive():
+            self.readThread.join()
 
-    def continual_read(self, event):
+    def continual_read(self, updateFunc, event):
         while event.is_set():
             line = self.readline()
             if line:
-                print(line)
-                AudioDeviceUpdater.set_device_volumes(AudioDeviceUpdater.extract_pot_values(line))
+                updateFunc(line)
 
 
-class AudioDeviceUpdater:
-    devices = []
-    interfaces = []
-    dialMappings = (
-        "Yeti Speakers",
-        "Realtek Speakers"
-    )
+class DeviceUpdater:
+    def __init__(self, deviceList):
+        self.all_devices = None
+        self.selected_interfaces = []
+        self.set_deviceList(deviceList)
 
-    def find_all_devices():
-        activeDevices = [device for device in AudioUtilities.GetAllDevices()
-                if device.state == AudioDeviceState.Active]
-        AudioDeviceUpdater.devices = [AudioDeviceUpdater.find_device(activeDevices, dm) for dm in AudioDeviceUpdater.dialMappings]
-        AudioDeviceUpdater.interfaces = [d._dev.Activate(IAudioEndpointVolume._iid_, CLSCTX_ALL, None).QueryInterface(IAudioEndpointVolume) for d in AudioDeviceUpdater.devices]
-        print(f"Found devices: {[i.FriendlyName for i in AudioDeviceUpdater.devices]}")
-        
-    def find_device(deviceList, searchStr):
-        searchTerms = searchStr.split()
-        for d in deviceList:
-            if all(term in d.FriendlyName for term in searchTerms):
-                return d
-        raise IndexError(f"Could not find device with search terms {searchTerms}")
-        
-    def extract_pot_values(rawBytes):
-        extractedVals = list(map(int, rawBytes.split(b"|")))[:2]
-        if len(extractedVals) != 2 or any(i == None for i in extractedVals):
-            raise RuntimeError()
-        return extractedVals
+    def set_deviceList(self, deviceList):
+        newInterfaces = list(map(self.open_device, deviceList))
+        self.all_devices = {d.FriendlyName: i for d,i in zip(deviceList, newInterfaces)}
 
-    def set_device_volumes(valueArray):
-        for i, value in enumerate(valueArray):
-            value = value/1024
+    def select_devices(self, deviceNameList):
+        self.selected_interfaces = []
+        for d in deviceNameList:
+            if d in self.all_devices:
+                self.selected_interfaces.append(self.all_devices[d])
+
+    def open_device(self, device):
+        return device._dev.Activate(
+            IAudioEndpointVolume._iid_,
+            CLSCTX_ALL,
+            None
+        ).QueryInterface(IAudioEndpointVolume)
+
+    def set_device_volumes(self, rawBytes):
+        def extract_pot_values(rawBytes):
+            return list(map(int, rawBytes.split(b"|")))
+
+        potValues = [i/1024 for i in extract_pot_values(rawBytes)]
+        for i, interface in enumerate(self.selected_interfaces):
             try:
-                interface = AudioDeviceUpdater.interfaces[i]
-                interface.SetMasterVolumeLevelScalar(round(value,3),None)
+                if i < len(potValues):
+                    interface.SetMasterVolumeLevelScalar(round(potValues[i],3),None)
 
-                if value < 6/1024:
+                if potValues[i] < 6/1024:
                     interface.SetMute(True, None)
                 else:
                     interface.SetMute(False, None)
-            except:
-                # device was most likely unplugged; skip
-                return
-
+            except COMError as ce:
+                print(f"Device #{i}: {ce.text}")
 
 if __name__ == "__main__":
     trayApp = TrayApplication()
